d920521323324b1cf1a2b714028aed8c258fe37a
serghei.takovski <serghei.takovski@softswiss.com>
2024-04-30 07:28:23 +0300

    New comments

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+// Albums list
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
